c594406c4fc69656f5e3342362cb948d980838aa
Vova <oparinv2005@mail.ru>
2025-03-30 00:11:57 +0300

    Добавлена база данных Добавлен класс для работы с базой данных Полностью переработана функция 'calculate'

--- Lab_3/Vova_variant/VovaLogic.py
+++ Lab_3/Vova_variant/VovaLogic.py
@@ -1,26 +1,52 @@
+import sqlite3
+
 from collections import deque
 
 
 class SlicePrognoses:
     """
-    Функция возвращает исходный список данных, дополненный прогнозами.
+    Функция возвращает копию исходного списка данных, дополненный прогнозами.
     Прогнозы вычисляются на основе буфера.
     """
-    @staticmethod
-    def calculate(data: list[float | int], n: int, years: int):
-        buffer: deque[float | int] = deque(data[-n:])
-        data_with_prognoses: list[float | int] = data.copy()
-        now_year: int = 0
 
-        while now_year < years:
-            data_with_prognoses.append(round(sum(buffer) / n, 2))
-            buffer.append(data_with_prognoses[-1])
+    @staticmethod
+    def calculate(data: list[tuple[str, int, int | float]], n: int, years: int) -> list[tuple[str, int, int | float]]:
+        buffer: deque[int | float] = deque((x[2] for x in data[-n:]))
+        new_data = data.copy()
+
+        for _ in range(years):
+            new_record = (new_data[-1][0], new_data[-1][1] + 1, round(sum(buffer) / n, 2))
+            new_data.append(new_record)
+
+            buffer.append(new_data[-1][2])
             buffer.popleft()
 
-            now_year += 1
+        return new_data
 
-        return data_with_prognoses
+
+class DatabaseWork:
+    """
+    Класс для работы с базой данных
+
+    Таблица tourists:
+    country text
+    year int
+    tourists_count int
+    """
+
+    def __init__(self):
+        self.connection = sqlite3.connect("var_11_database.db")
+        self.cursor = self.connection.cursor()
+
+    def get_country_data(self, country_name: str) -> list[tuple[str, int, int]]:
+        """Возвращает все данные из базы по выбранной стране"""
+        query = f"select * from tourists where country = {repr(country_name)}"
+        return self.cursor.execute(query).fetchall()
+
+    def get_all_data(self):
+        query = "select * from tourists"
+        return self.cursor.execute(query).fetchall()
 
 
 if __name__ == '__main__':
-    print(SlicePrognoses.calculate([60, 85, 80, 92, 88, 96], 3, 2))
+    pass
